a0daf8793ad61888583cdf325fed107973ef8b63
JaessScript <jaess.script@gmail.com>
2020-06-15 19:48:56 +0200

    Adding a .Gif and a text

--- sketch.js
+++ sketch.js
@@ -1,5 +1,22 @@
+let canvas;
+let p1, p2;
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+}
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	canvas = createCanvas(windowWidth, windowHeight);
+	canvas.position(0, 0);
+	canvas.style('z-index', '1');
+
+	p1 = createP('Germany is the 3rd most');
+	p2 = createP('surveilled country worlwide!');
+	p1.position(windowWidth / 8, windowHeight / 5);
+	p2.position(windowWidth / 8, (windowHeight / 5) + 70);
+	p1.style('font-size', '2em');
+	p2.style('font-size', '2em');
+	p1.style('font-family', '"geekyFont"');
 }
 
 function draw() {
