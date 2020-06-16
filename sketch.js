8ef393a7ce9c990ec0f6556f12ac6b7ae69ac255
JaessScript <jaess.script@gmail.com>
2020-06-16 16:06:14 +0200

    Fix responsiveness

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ let canvas;
 // let p1, p2;
 let font,
 	fontsize = 15;
+let x, y;
 
 function preload() {
 	// font = loadFont('assets/Fipps-Regular.otf');
@@ -10,6 +11,7 @@ function preload() {
 
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
+	setup();
 }
 
 function setup() {
@@ -32,6 +34,14 @@ function setup() {
 	// p1.style('font-size', '2em');
 	// p2.style('font-size', '2em');
 	// p1.style('font-family', '"geekyFont"');
+
+	if (windowWidth > 500) {
+		x = 300;
+		y = 150;
+	} else {
+		x = 100;
+		y = 100;
+	}
 }
 
 function draw() {
@@ -39,7 +49,16 @@ function draw() {
 	noStroke();
 	rectMode(CENTER);
 	fill(255);
-	rect(100, 150, windowWidth/3, (2*windowHeight)/5);
+	rect(x, y, windowWidth / 3, (2 * windowHeight) / 5);
 	fill(0);
-	text(s, 100, 150, windowWidth/5, windowHeight/3);
+	text(s, x, y, windowWidth / 5, windowHeight / 3);
+}
+
+function getRandomColor() {
+	let red = random(255);
+	let green = random(255);
+	let blue = random(255);
+	let c = color(red, green, blue);
+
+	return c;
 }
