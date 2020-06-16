af7214459adce56b81b0fe5ab1b271d9eabefa8c
JaessScript <jaess.script@gmail.com>
2020-06-16 15:09:54 +0200

    Adding the text

--- sketch.js
+++ sketch.js
@@ -1,5 +1,12 @@
 let canvas;
-let p1, p2;
+// let p1, p2;
+let font,
+	fontsize = 15;
+
+function preload() {
+	// font = loadFont('assets/Fipps-Regular.otf');
+	font = loadFont('assets/Pixellari.ttf');
+}
 
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight);
@@ -10,15 +17,28 @@ function setup() {
 	canvas.position(0, 0);
 	canvas.style('z-index', '1');
 
-	p1 = createP('Germany is the 3rd most');
-	p2 = createP('surveilled country worlwide!');
-	p1.position(windowWidth / 8, windowHeight / 5);
-	p2.position(windowWidth / 8, (windowHeight / 5) + 70);
-	p1.style('font-size', '2em');
-	p2.style('font-size', '2em');
-	p1.style('font-family', '"geekyFont"');
+	// textFont(font);
+	// textSize(width / 3);
+	// textAlign(LEFT, TOP);
+	// Set text characteristics
+	textFont(font);
+	textSize(fontsize);
+	textAlign(CENTER, CENTER);
+
+	// p1 = createP('Germany is the 3rd most');
+	// p2 = createP('surveilled country worlwide!'); 
+	// p1.position(windowWidth / 8, windowHeight / 5);
+	// p2.position(windowWidth / 8, (windowHeight / 5) + 70);
+	// p1.style('font-size', '2em');
+	// p2.style('font-size', '2em');
+	// p1.style('font-family', '"geekyFont"');
 }
 
 function draw() {
-
+	let s = "GERMANY IS THE 3rd WORLD'S MOST SURVEILLED COUNTRIES WITH 5.2 MILLIONS CCTVs!";
+	noStroke();
+	rect(15, 30, windowWidth/4, windowHeight/3);
+	fill(0);
+	text(s, 30, 30, windowWidth/5, windowHeight/3);
+	ellispe(30, 30, 50, 50);
 }
